83b1d63a4e520c2739ad1f5433f276e79ba5f9e7
Takumi Yamashiro <tarchin6061@gmail.com>
2024-10-09 23:21:07 +0900

    ページネーション実装

---
+++ search.php
@@ -0,0 +1,63 @@
+<?php get_header(); ?>
+    <main class="l-main">
+      <div class="p-main-title archive search">
+        <div class="p-main-title-bg">
+          <h2>Search:</h2>
+          <p><?php echo esc_html(get_search_query()); ?></p>
+        </div>
+      </div>
+      <article class="p-burger-information">
+        <div class="p-burger-information-text">
+          <h3>小見出しが入ります</h3>
+          <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+        </div>
+        <div class="p-burger-information-contents">
+          <?php if(have_posts()) :
+            while(have_posts()) :
+              the_post();?>
+          <div class="p-box--information-burger">
+            <?php the_post_thumbnail(); ?>
+            <div class="p-box-text-burger">
+              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+              <h4>小見出しが入ります</h4>
+              <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+              <a href="<?php the_permalink(); ?>" class="button">詳しく見る</a>
+            </div>
+          </div>
+          <?php endwhile; ?>
+          <?php else: ?>
+          <p>表示する記事がありません</p>
+          <?php endif; ?>
+        </div>
+        <?php if (function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+        <!-- <div class="p-archive-pagination">
+          <p>page 1/10</p>
+          <i class="fa-thin fa-angles-left"></i>
+          <ul class="p-pagination-list">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+          </ul>
+          <i class="fa-thin fa-angles-right"></i>
+        </div>
+        <div class="p-archive-pagination-sp">
+          <div class="p-archive-pagination-sp-back">
+            <i class="fa-thin fa-angles-left"></i>
+            <p>前へ</p>
+          </div>
+          <div class="p-archive-pagination-sp-next">
+            <p>次へ</p>
+            <i class="fa-thin fa-angles-right"></i>
+          </div>
+        </div> -->
+      </article>
+    </main>
+    <?php get_sidebar(); ?>
+  </div>
+  <?php get_footer(); ?>
---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form role="search" method="get" action="<?php echo home_url('/'); ?>" class="p-search-box">
+  <input type="text" name="s">
+  <button class="p-button search" type="submit">検索</button>
+</form>
